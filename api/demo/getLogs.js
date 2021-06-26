9139aa651f942ae6ab01828b4fa5ceb09e12b130
SahilRajpal-hub <sahilrajpal.31586@gmail.com>
2021-06-26 15:16:40 +0530

    random numbers added to graph

--- api/demo/getLogs.js
+++ api/demo/getLogs.js
@@ -1,7 +1,8 @@
 const UE = require("../models/UE");
+const NewUE = require("../models/newUe");
 
-module.exports = async function getLogs(hour = 1) {
+module.exports = async function getLogs(hour = 0.001) {
   var queryTime = new Date().getTime() - hour * 60 * 60 * 1000;
-  const ueLogs = await UE.find({ mongo_time: { $gte: queryTime } }).limit(50);
+  const ueLogs = await NewUE.find({ mongo_time: { $gte: queryTime } }).limit(50);
   return ueLogs;
 };
--- api/demo/logs.js
+++ api/demo/logs.js
@@ -4,6 +4,12 @@ const UE = require("../models/UE");
 const newUE = require("../models/newUe");
 const fetchUeLogs = require('../grpc/client');
 
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
 module.exports = function startLogging(socket) {
   // '*/n * * * * *'  ------>   for every nth second
   // '* */n * * * *'  ------>   for every nth minute
@@ -24,20 +30,22 @@ module.exports = function startLogging(socket) {
 
             const response1 = {
               timestamp: new Date().getTime(),
-              value: ue.time,
+              value: ue.time+getRandomNumber(0,10),
               zscore: 0
             }
             const response2 = {
               timestamp: new Date().getTime(),
-              value: ue.ueData.ulRate,
+              value: ue.ueData.ulRate+10*getRandomNumber(0,10),
               zscore: 0
             }
             const response3 = {
               timestamp: new Date().getTime(),
-              value: ue.ueData.dlRate,
+              value: ue.ueData.dlRate+1000000*getRandomNumber(0,10),
               zscore: 0
             }
-            console.log(ue.ueData)
+            console.log(ue.ueData.dlRate)
+            console.log(ue.ueData.ulRate)
+            console.log(ue.time)
             socket[0].emit("readingtime", JSON.stringify(response1));
             socket[1].emit("readinguplink", JSON.stringify(response2));
             socket[2].emit("readingdownlink", JSON.stringify(response3));
